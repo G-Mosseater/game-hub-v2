b5ca8239eea21f18e6e6b2ea3c1f2576b71cf9f1
Gabriel <gabriel.bachelor@gmail.com>
2025-06-17 13:53:37 +0000

    fetching game details

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,16 @@
-import NavBar from '../components/NavBar'
-import { Outlet } from 'react-router-dom'
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 const Layout = () => {
   return (
-<>
-<NavBar></NavBar>
-<Outlet></Outlet>
-</>  )
-}
+    <>
+      <NavBar />
+      <Box padding={5}>
+        <Outlet />
+      </Box>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
@@ -8,24 +7,29 @@ const axiosInstance = axios.create({
   },
 });
 
-
 class APIClient<T> {
   endpoint: string;
 
-  constructor( endpoint: string) {
-    this.endpoint=endpoint
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
   }
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
-    .get<FetchResponse<T>>(this.endpoint,config)
-    .then (res=> res.data)
-  }
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+  get = (id: number|string) => {
+    return  axiosInstance
+    .get<T>(this.endpoint + '/' + id)
+    .then (
+      (res) =>res.data
+    )
+  };
 }
-export  default APIClient
+export default APIClient;
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
-  next: string | null
-
+  next: string | null;
 }
