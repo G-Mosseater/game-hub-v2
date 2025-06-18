b21aced0fe9554048d92f2698e0163854456e6ff
Gabriel <gabriel.bachelor@gmail.com>
2025-06-18 20:25:18 +0000

    fixed navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,7 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import { useGameQueryStore } from "../store";
 
 // interface Props {
@@ -30,12 +31,14 @@ import { useGameQueryStore } from "../store";
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSeachText = useGameQueryStore(s => s.setSearchText);
+  const navigate = useNavigate()
 
 
   return (
     <form onSubmit={(event) => {
       event.preventDefault();
       if (ref.current) setSeachText(ref.current.value);
+      navigate('/')
     }}>
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
