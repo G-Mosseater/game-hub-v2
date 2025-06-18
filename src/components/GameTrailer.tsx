71e1be352496e797292df61110c13b9a307451b6
Gabriel <gabriel.bachelor@gmail.com>
2025-06-18 19:39:35 +0000

    game trailer build

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,23 @@
+import useTrailers from "../hooks/useTrailers";
+import { Text } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+  console.log(data?.results[0].name);
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[2];
+  return first ? (
+  <>
+  <Text>{first.name}</Text>
+  <video poster={first.preview} src={first.data[480]} controls/> 
+  </>) : null
+};
+
+export default GameTrailer;
